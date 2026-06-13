086ad81dd2d9fb76bd17929393560e0f72078acc
devidibhavyareddy <devidibhavyareddy@gmail.com>
2026-06-13 23:01:36 +0530

    Fix CORS origin URL by removing trailing slash

--- backend/server.js
+++ backend/server.js
@@ -26,7 +26,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://individual-project-livid.vercel.app/"
+      "https://individual-project-livid.vercel.app"
     ],
     credentials: true,
   })
