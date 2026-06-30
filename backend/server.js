23d6621c7c67ea78e7b0f20d333cd87887a674c0
devidibhavyareddy <devidibhavyareddy@gmail.com>
2026-06-30 22:04:31 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -26,7 +26,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://paw-rescue-rouge.vercel.app"
+      "https://paw-rescue-tau.vercel.app/"
     ],
     credentials: true,
   })
